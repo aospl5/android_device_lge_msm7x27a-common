f57a0761093c6d7a6cd9d286eac8188f4c36ac6c
Aidas Lukošius <aidaslukosius75@gmail.com>
2015-01-18 18:13:28 +0200

    add dex

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -121,7 +121,14 @@ HTTP := chrome
 
 # ART
 MALLOC_IMPL := dlmalloc
-
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+ ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+  ifeq ($(WITH_DEXPREOPT),)
+   WITH_DEXPREOPT := true
+  endif
+ endif
+endif
 DONT_DEXPREOPT_PREBUILTS := true
 
 # Radio
