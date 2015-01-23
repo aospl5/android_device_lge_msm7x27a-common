eb6d7b61ff6f9e1b5747cf928e7e4c29a549c14c
aidasaidas75 <aidaslukosius75@gmail.com>
2015-01-23 20:46:59 +0200

    msm7x27a-common:test2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,7 @@ TARGET_NEEDS_NON_PIE_SUPPORT := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN              := false
 BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
