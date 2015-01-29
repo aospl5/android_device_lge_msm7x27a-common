a2e530d04cc795d0df2ba097a24672d5a349fc72
Aidas Lukošius <aidaslukosius75@gmail.com>
2015-01-29 16:26:21 +0200

    Update msm7x27a-common.mk

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -137,14 +137,15 @@ PRODUCT_PACKAGES += \
 
 # Radio properties
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.telephony.default_network=0 \
-   ro.telephony.call_ring.multiple=0 \
-   telephony.lteOnGsmDevice=0 \
-   rild.libpath=/system/lib/libril-qc-qmi-1.so \
-   rild.libargs=-d/dev/smd0 \
-   ril.subscription.types=NV,RUIM \
-   DEVICE_PROVISIONED=1 \
-   persist.radio.apm_sim_not_pwdn=1
+    ro.telephony.ril_class=LGEQualcommUiccRIL \
+    ro.telephony.default_network=0 \
+    ro.telephony.call_ring.multiple=0 \
+    telephony.lteOnGsmDevice=0 \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    rild.libargs=-d/dev/smd0 \
+    ril.subscription.types=NV,RUIM \
+    DEVICE_PROVISIONED=1 \
+    persist.radio.apm_sim_not_pwdn=1
 
 # Media codecs
 PRODUCT_COPY_FILES += \
